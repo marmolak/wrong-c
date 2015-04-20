b9e30d69ccd60b86a4d5ff5b1a36d4dd989c4893
Robin Hack <rhack@redhat.com>
2015-04-20 16:00:49 +0200

    array: Add forgotten changes.

--- c++/codefights/array.cpp
+++ c++/codefights/array.cpp
@@ -4,12 +4,27 @@
 #include <stdio.h>
 using namespace std;
 
+/*
+You are given 2 numbers a and b where 0 <= a <= b. Imagine you construct an array of all the numbers
+from a to b inclusive. You need to count the number of 1's in the binary representations of all the numbers in the array.
+
+Example given a = 2 and b = 7 the array is: [2, 3, 4, 5, 6, 7]. Converting the numbers to binary,
+we get [10, 11, 100, 101, 110, 111], which contains 1 + 2 + 1 + 2 + 2 +3 = 11 1s.
+
+First draft:
+	Execution time limit exceeded!!!
+
+Second draft:
+	in progress
+*/
+
 int bit1Count(int a, int b) {
-	unsigned int y, cnt = 0;
+	register unsigned int y, cnt = 0;
 	for(; a <= b ; ++a) {
 		y = 1;
-		while (y & ~(UINT_MAX + 1)) {
+		while (y & UINT_MAX) {
 			if (a & y) cnt++;
+			/* Let y overflow) to 0 */
 			y <<= 1;
 		}
 	}
